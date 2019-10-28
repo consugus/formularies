dd7a6b57b34b3bdfb5ee9fcd2bb39fd76977a8f5
Gustavo <consugus@gmail.com>
2019-10-28 12:30:42 -0300

    Advances with somethings, upt to video 185

---
+++ src/app/components/template/template.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: []
+})
+export class TemplateComponent {
+
+  constructor() { }
+
+
+
+  guardar(){
+    console.log('El formulario se envió');
+
+  }
+
+}
